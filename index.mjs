859fe1f080d0740468243193fd21cb32a720fb43
aghArdeshir <ardeshirizadinl@gmail.com>
2023-02-28 19:34:36 +0100

    add canvas creator

---
+++ index.mjs
@@ -0,0 +1,20 @@
+import { colorPalette } from "./color-palette.mjs";
+
+document.body.style.backgroundColor = colorPalette.one;
+
+function createCanvas() {
+  const canvas = document.createElement("canvas");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  document.body.appendChild(canvas);
+
+  window.addEventListener("resize", () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  });
+}
+
+const playerCanvas = createCanvas();
+const bulletsCanvas = createCanvas();
+const enemiesCanvas = createCanvas();
