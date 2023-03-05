415c3e17aeb9cd7b5b2956ebf7d850e4f5bec737
aghArdeshir <ardeshirizadinl@gmail.com>
2023-03-05 23:20:00 +0100

    add type definitions for findLastIndex

---
+++ global.d.ts
@@ -0,0 +1,10 @@
+declare global {
+  interface Array<T> {
+    findLastIndex(
+      predicate: (value: T, index: number, obj: T[]) => unknown,
+      thisArg?: any
+    ): number;
+  }
+}
+
+export {};
